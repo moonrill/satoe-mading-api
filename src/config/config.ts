85e29fbe909dd87e8a99183b6cf49be3677a60bd
moonrill <arilramadani245@gmail.com>
2024-05-22 20:29:46 +0700

    feat: change app to HMR webpack

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ export default () => ({
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    entities: ['dist/**/*.entity{.ts,.js}'],
     autoLoadEntities: true,
     synchronize: true,
     logging: true,
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -22,6 +24,11 @@ async function bootstrap() {
   const port = configService.get('port') || 3000;
 
   await app.listen(port);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 
   Logger.log('Application listening on port ' + port);
 }
